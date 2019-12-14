7eec85b1f2503db77e1a4f0423183eb07ba2eb4e
antonS <so4usertrue@gmail.com>
2019-12-14 22:12:45 +0300

    Create model user and jwt

--- server/app.js
+++ server/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const passportStrategy = require('./middleware/passport-strategy');
 const keys = require('./keys');
 const authRoutes = require('./routes/auth.routes');
 
 mongoose.connect(keys.MONGO_URI)
   .then(() => console.log('mongoDb connected'))
   .catch(err => console.log(err));
+
+app.use(passport.initialize());
+passport.use(passportStrategy);
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
